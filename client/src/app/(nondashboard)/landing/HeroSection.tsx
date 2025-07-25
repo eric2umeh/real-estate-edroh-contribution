e46631fe13ecb5d351709448a91e29def705a073
Eric Umeohabike <eric2umeh@yahoo.com>
2025-07-25 11:12:07 +0100

    text size mobile

--- client/src/app/(nondashboard)/landing/HeroSection.tsx
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -64,10 +64,10 @@ const HeroSection = () => {
         className="absolute top-1/3 transform -translate-x-1/2 -translate-y-1/2 text-center w-full"
       >
         <div className="max-w-4xl mx-auto px-16 sm:px-12">
-          <h1 className="text-5xl font-bold text-white mb-4">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-4">
             Start your journey to finding the perfect place to call home
           </h1>
-          <p className="text-xl text-white mb-8">
+          <p className="text-base sm:text-lg md:text-xl text-white mb-8">
             Explore our wide range of rental properties tailored to fit your
             lifestyle and needs!
           </p>
